2698a3ad46fddc61818aab0b7ac69925b11e3b5d
Katie Miller <katie@codemiller.com>
2014-05-12 18:11:57 +1000

    Updating to match cartridge code changes and remove deprecated functions

--- Main.hs
+++ Main.hs
@@ -1,21 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty
-import System.Environment
-import Control.Monad
-import Network.Wai.Middleware.RequestLogger
 import Data.Default (def)
-import Network.Wai.Handler.Warp (settingsPort, settingsHost)
-import Data.Conduit.Network
+import Data.String (fromString)
+import Data.Monoid (mconcat)
 
-opts :: String -> Int -> Options
-opts ip port = def { verbose = 0
-                   , settings = (settings def) { settingsHost = Host ip, settingsPort = port }
-               }
+import System.Environment (getArgs)
+
+import Network.Wai.Handler.Warp (setHost, setPort, defaultSettings)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+
+import Web.Scotty (Options(..), scottyOpts, middleware
+                  , get, post, param, text, html)
 
 main :: IO ()
-main = do 
-     (ip:port:args) <- getArgs
-     scottyOpts (opts ip $ read port) $ do
-         middleware logStdoutDev
-         get "/" $ text "Hello World!"
+main = do
+    opts <- commandLineOptions
+    scottyOpts opts $ do
+        middleware logStdoutDev
+        get "/" $ text "Hello World!"
+        get "/:word" $ do
+            beam <- param "word"
+            html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
 
+commandLineOptions :: IO Options
+commandLineOptions = do
+    (ip:port:_) <- getArgs
+    let sets = setPort (read port) . setHost (fromString ip) $ defaultSettings
+    return $ def { verbose = 0, settings = sets }
